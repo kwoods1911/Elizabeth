caaebeb433f4d8dba4a3432a19458ce28a3de7e4
Khari <khariwoods3@gmail.com>
2022-05-03 23:17:54 -0300

    Inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./assets/*.liquid",
+    "./layout/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./templates/*.liquid",
+    "./templates/customers/*.liquid",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
